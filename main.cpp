7e42691c2464eb3f4dc8abbc88d5b2eaf6b43e8a
Swordword <massivejohn123@gmail.com>
2020-10-07 14:56:55 +0800

    格式化

--- main.cpp
+++ main.cpp
@@ -2,12 +2,10 @@
 // Created by 张建宇 on 2020/10/2.
 //
 #include "Cpp/common.h"
-#include "Cpp/771.宝石与石头.cpp"
+#include "Cpp/2.两数相加.cpp"
 
 int main()
 {
     Solution Foo;
-    int res = Foo.numJewelsInStones("aA", "aAAbbbb");
-    cout << res << endl;
     return 0;
 }
