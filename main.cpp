16020c149262c794126076b407f927f45facc680
John Massive <massivejohn123@gmail.com>
2020-10-12 18:15:57 +0800

    c++ 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 int main()
 {
     Solution Foo;
-    cout << Foo.lengthOfLongestSubstring("hello") << endl;
+    vector<int> nums{2, 7, 11, 15};
+    int target = 9;
+    vector<int> res = Foo.twoSum(nums, target);
+    cout << res.size() << endl;
     return 0;
 }
