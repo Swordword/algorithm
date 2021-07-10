1a44d82222251500af2e4a3086361cad6b16d2c7
Swordword <massivejohn123@gmail.com>
2021-07-10 23:40:34 +0800

    Create 5793. 迷宫中离入口最近的出口.cpp

---
+++ Cpp/5793. 迷宫中离入口最近的出口.cpp
@@ -0,0 +1,12 @@
+// https://leetcode-cn.com/contest/biweekly-contest-56/problems/nearest-exit-from-entrance-in-maze/
+
+#include "common.h"
+
+class Solution
+{
+public:
+  int nearestExit(vector<vector<char>> &maze, vector<int> &entrance)
+  {
+    
+  }
+};
