fb6ba8598ae2a3a6d383900b658f47b4acf1dad9
Swordword <massivejohn123@gmail.com>
2021-07-11 08:21:28 +0800

    update

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,18 +1,18 @@
 #include "common.h"
-#include "739.每日温度.cpp"
+#include "5801. 消灭怪物的最大数量.cpp"
 
 int main()
 {
-
+  int a = 3;
+  int b = 4;
+  double c = a / (b / 1.0);
+  cout << "c: " << c << endl;
   Solution foo;
-  vector<int> data{73, 74, 75, 71, 69, 72, 76, 73};
-  
-  auto res = foo.dailyTemperatures(data);
-  for (auto &&i : res)
-  {
-    cout << i << ' ';
-  }
-  cout << endl;
+  vector<int> dist{1, 3, 4},
+      speed{1, 1, 1};
+  auto res = foo.eliminateMaximum(dist, speed);
+
+  cout << "res = " << res << endl;
 
   return 0;
 }
