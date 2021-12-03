4262ff56b7d528d5dbd9c23680eb9d1d5dff1bc4
Swordword <massivejohn123@gmail.com>
2021-12-03 19:12:29 +0800

    Update main.cpp

--- data-struction/tree/main.cpp
+++ data-struction/tree/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+typedef struct TreeNode *BinTree;
+typedef BinTree Position;
+struct TreeNode
+{
+  int Data;
+  BinTree Left;
+  BinTree Right;
+};
+// 递归遍历
+void PreOrderTraversal(BinTree BT)
+{
+  if (BT)
+  {
+    printf("%d", BT->Data);
+    PreOrderTraversal(BT->Left);
+    PreOrderTraversal(BT->Right);
+  }
+};
+
+// 非递归遍历
+void PreOrderTraversalWithStack(BinTree BT)
+{
+  BinTree T = BT;
+  
+  Stack S = CreateStack(MaxSize);
+  while (T|| !IsEmpty(S))
+  {
+    While(T){
+      printf('%5d', T->Data);
+      Push(S,T);
+      T = T-> Left;
+    }
+    if(!IsEmpty(S)){
+      T = Pop(S);
+      T = T->Right;
+    }
+  }
+  
+}
+
 int main()
 {
   return 0;
