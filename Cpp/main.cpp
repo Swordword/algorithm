2c0733d5df652c21562ee31acd47f234aa872d81
Swordword <massivejohn123@gmail.com>
2021-12-19 11:37:50 +0800

    Update main.cpp

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,6 +1,10 @@
-#include "common.h"
-#include "5956. 找出数组中的第一个回文字符串.cpp"
+#include "common.h";
+#include "5956. 找出数组中的第一个回文字符串.cpp";
 
 int main()
 {
+  Solution foo;
+  vector<string> vstr{"abc", "car", "ada", "racecar", "cool"};
+  auto aa = foo.firstPalindrome(vstr);
+  cout << "res: " << aa << endl;
 }
