dafe02fb14a5efeca6ee7cc6d6a9b135f09db733
Swordword <massivejohn123@gmail.com>
2021-12-19 10:56:22 +0800

    5956. 找出数组中的第一个回文字符串

---
+++ Cpp/5956. 找出数组中的第一个回文字符串.cpp
@@ -0,0 +1,15 @@
+// https://leetcode-cn.com/contest/weekly-contest-272/problems/find-first-palindromic-string-in-the-array/
+
+#include "common.h";
+class Solution
+{
+public:
+  string firstPalindrome(vector<string> &words)
+  {
+    for (auto i = 0; i < words.size(); i++)
+    {
+      string str = words[i];
+      
+    }
+  }
+};
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,11 +1,6 @@
 #include "common.h"
-#include "5830. 三除数.cpp"
+#include "5956. 找出数组中的第一个回文字符串.cpp"
 
 int main()
 {
-  int n = 4;
-  Solution foo;
-  auto res = foo.isThree(n);
-  cout << "res: " << res << endl;
-  return 0;
 }
