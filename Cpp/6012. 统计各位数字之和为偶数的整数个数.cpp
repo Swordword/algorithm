bcb9d3c0f314f889d7ec4100c3e7b40c2d12f849
Swordword <massivejohn123@gmail.com>
2022-02-20 11:54:26 +0800

    Create 6012. 统计各位数字之和为偶数的整数个数.cpp

---
+++ Cpp/6012. 统计各位数字之和为偶数的整数个数.cpp
@@ -0,0 +1,10 @@
+// https://leetcode-cn.com/contest/weekly-contest-281/problems/count-integers-with-even-digit-sum/
+
+#include "common.h";
+
+class Solution {
+public:
+    int countEven(int num) {
+        
+    }
+};
