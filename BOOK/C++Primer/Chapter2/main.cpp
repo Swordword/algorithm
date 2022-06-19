9f67d29ef0726eb3f43cc2569c6609d816d95dd2
张建宇 <massivejohn123@gmail.com>
2022-06-19 23:38:59 +0800

    update

---
+++ BOOK/C++Primer/Chapter2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+  int a1 = 40000 * 40000;
+  int a2 = 50000 * 50000;
+  cout << a1 << endl;
+  cout << a2 << endl;
+  return 0;
+}
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,19 +1,11 @@
 // #include "common.h";
-#include "./5268. 找出两数组的不同.cpp"
+#include "./6037. 按奇偶性交换后的最大数字.cpp"
 
 int main()
 {
   Solution s;
-  vector<int> nums1 = {1,2,3};
-  vector<int> nums2 = {2, 5, 6};
-  vector<vector<int>> answer = s.findDifference(nums1, nums2);
-  for (auto vi : answer)
-  {
-    for (auto i : vi)
-    {
-      cout << i << " ";
-    }
-    cout << endl;
-  }
+  int num = 1234;
+  int res = s.largestInteger(num);
+  cout << res << endl;
   return 0;
 }
