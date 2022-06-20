a50cd7e0db9615f852a752a31a08a5cd1ecb64c7
张建宇 <massivejohn123@gmail.com>
2022-06-20 08:08:29 +0800

    Create foo.cpp

---
+++ MOOC/B站-CSAPP/foo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+typedef struct
+{
+  int a[2];
+  double d;
+} struct_t;
+
+double fun(int i)
+{
+  volatile struct_t s;
+  s.d = 3.14;
+  s.a[i] = 1073741824;
+  return s.d;
+}
+
+int main()
+{
+  auto res = fun(3);
+  cout << res << endl;
+  return 0;
+}
